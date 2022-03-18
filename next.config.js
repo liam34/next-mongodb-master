824ca7652bc37fdd6a42ac09a4ea6e532ae340b3
liam34 <indemandcoding@gmail.com>
2022-03-17 23:01:52 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     env: {
-        MONGO_URI: "mongodb+srv://liam34:VC63ft5c34@clusternextmongodb.a7ghi.mongodb.net/notes-next-db?retryWrites=true&w=majority"
+        MONGO_URI: ""
     }
 }
